259251b6133fea148b432d0d72b28242e5530678
liuyan <liuyanhp@gmail.com>
2017-07-12 16:39:57 +0800

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -30,9 +30,9 @@ if [ "$OAUTH_ADDITIONAL_PARAMS" != "**None**" ]; then
     replace_in_index "additionalQueryStringParams: {}" "additionalQueryStringParams: {$OAUTH_ADDITIONAL_PARAMS}"
 fi
 
-if [[ -f $SWAGGER_JSON ]]; then
-  cp $SWAGGER_JSON $NGINX_ROOT
-  REL_PATH="/$(basename $SWAGGER_JSON)"
+if [[ -f $SWAGGER_PATH ]]; then
+  cp $SWAGGER_JSON/*.yaml $NGINX_ROOT
+  REL_PATH="/$(basename $SWAGGER_PATH)/baobab.yaml"
   sed -i "s|http://petstore.swagger.io/v2/swagger.json|$REL_PATH|g" $INDEX_FILE
   sed -i "s|http://example.com/api|$REL_PATH|g" $INDEX_FILE
 else
